e748300378e4b82fb8cbb59b9ec598890a70ffa9
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2026-05-27 10:46:46 +0530

    removed unused js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
-
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -20,21 +9,19 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-
-            if (id.includes('react-router-dom') || id.includes('react-router-hash-link')) {
-              return 'router'
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react-router-dom") ||
+              id.includes("react-router-hash-link")
+            ) {
+              return "router";
             }
 
-            if (id.includes('framer-motion')) {
-              return 'motion'
+            if (id.includes("react")) {
+              return "react-vendor";
             }
 
-            if (id.includes('react')) {
-              return 'react-vendor'
-            }
-
-            return 'vendor'
+            return "vendor";
           }
         },
       },
@@ -42,4 +29,4 @@ export default defineConfig({
 
     chunkSizeWarningLimit: 600,
   },
-})
+});
